54e1e7b1cd51f43cc92a518cabee61ad1b36277e
epyc <arash.tchoupani@gmail.com>
2009-10-23 00:20:57 -0400

    new url for index

--- urls.py
+++ urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Example:
     (r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root': '/Users/atchoupani/workspace/pyjobs/static'}),
     (r'^jobs/', include('pyjobs.job.urls')),
-
+    (r'^$', 'pyjobs.job.views.index'),
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
