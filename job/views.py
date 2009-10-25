b3f527a1d98d6a2efffc50ef9d71a2ad4fded586
epyc <arash.tchoupani@gmail.com>
2009-10-25 15:13:52 -0400

    change the index view (filter by city)

--- job/views.py
+++ job/views.py
@@ -3,12 +3,16 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from pyjobs.job import models
 
-def index(request):
-  _jobs = models.Job.objects.all()
-  return render_to_response('job/list.html', {'jobs': _jobs})
+def index(request, city):
+  if city:
+    _jobs = models.Job.objects.filter(location = city)
+  else:
+    _jobs = models.Job.objects.all()
+  return render_to_response('job/list.html', {'jobs': _jobs, 'cities':['montreal','sfbay'],'city': city},)
 
 
 def job_detail(request, job_id):
   _job = models.Job.objects.get(id=job_id)
   return render_to_response('job/detail.html', {'job':_job})
+
 
