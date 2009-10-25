fb40aa91c14d055126b71c9d4caf27cd64df8cfa
epyc <arash.tchoupani@gmail.com>
2009-10-25 15:16:11 -0400

    small improvements in the crawler code, still feels ugly (embarassing)

--- utils/crawlers.py
+++ utils/crawlers.py
@@ -8,21 +8,23 @@ craigslist_base = "http://%s.craigslist.org"
 query = "/search/sof?query=python&catAbbreviation=sof"
 
 def crawl_cl(city):
-  url = (craigslist_base + query) % city
+  url = craigslist_base % city + query
+  print url
   data = urllib2.urlopen(url)
   soup = BeautifulSoup( data.read() )
   
   for job in soup.findAll('a', attrs={'href': re.compile(".*/sof/[0-9]*.html")}):
+     job_url = craigslist_base %city + job['href']
      
-     job_url = CRAIGSLIST_BASE + job['href']
+     print job_url
      job_post_data = urllib2.urlopen(job_url).read()
      job_soup = BeautifulSoup(job_post_data)
 
      body = job_soup.find('div', {"id":"userbody"})
      title = strip_tags(unicode(job_soup.find('h2')))
      date_added = clget_date(job_soup) 
-
-     newjob = models.Job(title = title, content = body, location = city, date_added = date_added)
+     newjob = models.Job(title = title, content = unicode(body), location = city, date_added = date_added,
+                         external_reference = job_url )
      newjob.save()
 
 
@@ -30,8 +32,7 @@ def clget_date(job_soup):
   f = unicode(job_soup).find('Date:')
   date_added = re.compile("Date: (.*)\<br.*>").match(unicode(job_soup)[f:f+50]).groups()[0]
   d = datetime.datetime.strptime(date_added.rstrip()[:10], '%Y-%m-%d')
-  return date_added
-  
+  return d 
 
 def strip_tags(value):
     "Return the given HTML with all tags stripped."
